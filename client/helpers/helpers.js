62748088b839060bd56c6347e2f36b27dba991e9
Prashant <kv.prshnt@gmail.com>
2013-11-03 03:55:10 +0530

    Add subtext in call-for-action buttons
    Style subtext
    Check for schedule config to show call for action

--- client/helpers/helpers.js
+++ client/helpers/helpers.js
@@ -2,11 +2,17 @@ Handlebars.registerHelper("activeLink", function (nav) {
   return Session.equals("activeLink", nav.toLocaleLowerCase()) ? "active" : "";
 });
 
-Handlebars.registerHelper('CFA', function(buttonText) {
+Handlebars.registerHelper('CFA', function(buttonText, subText) {
+	subText = '(Get started now)'
   return new Handlebars.SafeString(
     '<div class="row">' +
 		'<div class="large-5 small-6 large-centered small-centered columns alignMiddle">' +
-			'<a class="button success radius" href="http://devthonhyd.explara.com" target="_blank">' +  buttonText + '</a>' + 
+			'<a class="button success radius" href="http://devthonhyd.explara.com" target="_blank">' +  
+				buttonText +
+				'<p class="subtext">'+ 
+				subText +
+				'</p>' + 
+			'</a>' + 
 		'</div>' +
 	'</div>'
   );
