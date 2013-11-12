9082585735014d03157e190d5c106023668fe04f
Prashant <kv.prshnt@gmail.com>
2013-11-12 15:50:25 +0530

    Fixes usability and design issues
    Changes button text and pointing links
    Adds SVG logo instead of PNG
    Fixes smaller media viewport issues
    Fixes color scheme for all elements
    Fixes #7: Keep call for action same across all buttons

--- client/helpers/helpers.js
+++ client/helpers/helpers.js
@@ -2,12 +2,17 @@ Handlebars.registerHelper("activeLink", function (nav) {
   return Session.equals("activeLink", nav.toLocaleLowerCase()) ? "active" : "";
 });
 
-Handlebars.registerHelper('CFA', function(buttonText, subText) {
-	subText = '(Get started now)'
+Handlebars.registerHelper('CFA', function(type, buttonText, href, subText) {
+  if (subText) {
+	subText = '';
+  }
+  if (!type) {
+  	type = 'secondary';
+  }
   return new Handlebars.SafeString(
     '<div class="row">' +
-		'<div class="large-5 small-6 large-centered small-centered columns alignMiddle">' +
-			'<a class="button success radius" href="http://devthonhyd.explara.com" target="_blank">' +  
+		'<div class="large-5 small-5 large-centered small-centered columns alignMiddle">' +
+			'<a class="button ' + type + ' radius" href="' + href + '" target="_blank">' +  
 				buttonText +
 				'<p class="subtext">'+ 
 				subText +
