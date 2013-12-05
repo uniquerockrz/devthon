f47f9a38407244b37a94813ffb92be8dc7141e19
Prashant <kv.prshnt@gmail.com>
2013-12-05 19:49:22 +0530

    Changes about and adds social actions
    Adds fb like button
    Adds twitter follow button
    Changes the about content
    Removes team subheaders
    Adds foundation icon set

--- client/views/includes/navigation.js
+++ client/views/includes/navigation.js
@@ -1,3 +1,14 @@
+Template.tweeter.rendered = function() {
+	return !function(d,s,id) {
+		var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+		if(!d.getElementById(id)){
+			js=d.createElement(s);
+			js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+			fjs.parentNode.insertBefore(js,fjs);
+		}
+	}(document, 'script', 'twitter-wjs');
+}
+
 Template.navigation.events({
 
 	'click a.nav-blog': function(e) {
